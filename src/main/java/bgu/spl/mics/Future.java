bfd7173ac14f5f8de2fac14729c4861f875deab2
סער ויז <saarwi@post.bgu.ac.il>
2024-12-28 10:28:22 +0200

    upload the project

---
+++ src/main/java/bgu/spl/mics/Future.java
@@ -0,0 +1,94 @@
+package bgu.spl.mics;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A Future object represents a promised result - an object that will
+ * eventually be resolved to hold a result of some operation. The class allows
+ * Retrieving the result once it is available.
+ * 
+ * Only private methods may be added to this class.
+ * No public constructor is allowed except for the empty constructor.
+ */
+public class Future<T> {
+	
+	/**
+	 * This should be  the only public constructor in this class.
+	 */
+	T result;
+	boolean isDone;
+	public Future() {
+		result=null;
+		isDone=false;
+	}
+	
+	/**
+     * retrieves the result the Future object holds if it has been resolved.
+     * This is a blocking method! It waits for the computation in case it has
+     * not been completed.
+     * <p>
+     * @return return the result of type T if it is available, if not wait until it is available.
+     * 	       
+     */
+	public T get() throws InterruptedException {
+		while(!isDone){
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				continue;//++++++++
+			}
+		}
+		//relese the thread
+		return result;
+	}
+	
+	/**
+     * Resolves the result of this Future object.
+     */
+	public void resolve (T result) {
+		this.result=result;
+		isDone=true;
+		this.notify();
+	}
+	
+	/**
+     * @return true if this object has been resolved, false otherwise
+     */
+	public boolean isDone() {
+		return  isDone;
+	}
+	
+	/**
+     * retrieves the result the Future object holds if it has been resolved,
+     * This method is non-blocking, it has a limited amount of time determined
+     * by {@code timeout}
+     * <p>
+     * @param timout 	the maximal amount of time units to wait for the result.
+     * @param unit		the {@link TimeUnit} time units to wait.
+     * @return return the result of type T if it is available, if not, 
+     * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
+     *         elapsed, return null.
+     */
+	public T get(long timeout, TimeUnit unit) {
+		long gTime=unit.toMillis(timeout);
+		long startTime = System.currentTimeMillis();
+		synchronized (this){
+			while (!isDone){
+				long pasTime = System.currentTimeMillis() - startTime;
+				long remTime = gTime - pasTime;
+				if(gTime<pasTime){
+					return null;
+				}
+                try {
+                    this.wait(remTime);
+                } catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					continue;//++++++++
+                }
+            }
+		}
+
+		return result;
+	}
+}
---
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -0,0 +1,132 @@
+package bgu.spl.mics;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.*;
+/**
+ * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
+ * Write your implementation here!
+ * Only one public method (in addition to getters which can be public solely for unit testing) may be added to this class
+ * All other methods and members you add the class must be private.
+ */
+public class MessageBusImpl implements MessageBus {
+	private static MessageBusImpl instance = null;
+	private ConcurrentHashMap<Event, LinkedBlockingQueue<MicroService>> eventSubscribers;
+	private ConcurrentHashMap<Broadcast, LinkedBlockingQueue<MicroService>> broadcastSubscribers;
+	private ConcurrentHashMap<MicroService, LinkedBlockingQueue<Message>> numMicro;
+	private ConcurrentHashMap<MicroService, Future> micrtFuture;
+	private ConcurrentHashMap<Event, MicroService> micrtComplete;//++++++++++++
+	private ConcurrentHashMap<Event, MicroService> eActuall;
+
+
+	public MessageBusImpl() {
+		this.eventSubscribers = new ConcurrentHashMap<>();
+		this.broadcastSubscribers = new ConcurrentHashMap<>();
+		this.numMicro = new ConcurrentHashMap<>();
+		this.micrtFuture = new ConcurrentHashMap<>();
+		this.micrtComplete = new ConcurrentHashMap<>();//++++++++++++
+		this.eActuall = new ConcurrentHashMap<>();
+		instance = this;
+	}
+
+	public MessageBusImpl(int eventSus, int brodSus, int numMicro, int micrtFuture, int micrtComplete, int eActuall) {
+		this.eventSubscribers = new ConcurrentHashMap<>(eventSus);
+		this.broadcastSubscribers = new ConcurrentHashMap<>(brodSus);
+		this.numMicro = new ConcurrentHashMap<>(numMicro);
+		this.micrtFuture = new ConcurrentHashMap<>(micrtFuture);
+		this.micrtComplete = new ConcurrentHashMap<>(micrtComplete);//++++++++++++
+		this.eActuall = new ConcurrentHashMap<>(eActuall);
+		instance = this;
+	}
+
+	@Override
+	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		eventSubscribers.get(type.getClass()).add(m);
+	}
+
+	@Override
+	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+		this.broadcastSubscribers.get(type.getClass()).add(m);
+	}
+
+	@Override
+	public <T> void complete(Event<T> e, T result) {
+		micrtFuture.get(eActuall.get(e)).resolve(result);
+		micrtComplete.put(e, eActuall.get(e));//++++++++++++
+		eActuall.remove(e, eActuall.get(e));
+		micrtFuture.remove(eActuall.get(e), micrtFuture.get(eActuall.get(e)));
+
+
+	}
+
+	@Override
+	public void sendBroadcast(Broadcast b) {
+		broadcastSubscribers.get(b.getClass()).forEach(m -> {
+			numMicro.get(m).add(b);
+		});
+
+	}
+
+
+	@Override
+	public <T> Future<T> sendEvent(Event<T> e) {
+		try {
+			MicroService m = eventSubscribers.get(e.getClass()).take();
+			numMicro.get(m).add(e);
+			eventSubscribers.get(e.getClass()).add(m);
+			eActuall.put(e, m);
+			Future<T> future = new Future<>();
+			micrtFuture.put(m, future);
+			return future;
+		} catch (InterruptedException e1) {
+			return null;
+		}
+	}
+
+	@Override
+	public void register(MicroService m) {
+		LinkedBlockingQueue<Message> queue = new LinkedBlockingQueue<>();
+		numMicro.put(m, queue);
+	}
+
+	@Override
+	public void unregister(MicroService m) {
+		numMicro.remove(m);
+		eventSubscribers.forEach((k, v) -> {
+			if (v.contains(m)) {
+				v.remove(m);
+			}
+		});
+		broadcastSubscribers.forEach((k, v) -> {
+			if (v.contains(m)) {
+				v.remove(m);
+			}
+		});
+		//++++++change the microservis that hundlle evant if it there is one
+
+	}
+
+	@Override
+	public Message awaitMessage(MicroService m) throws InterruptedException {
+
+		LinkedBlockingQueue<Message> queue = numMicro.get(m);
+		//take of from all the messege queue
+		while (queue.isEmpty()) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+
+		}
+		return queue.take();
+	}
+
+
+	public static synchronized MessageBusImpl getInstance() {
+		if (instance == null) {
+			instance = new MessageBusImpl();
+		}
+		return instance;
+
+	}
+}
